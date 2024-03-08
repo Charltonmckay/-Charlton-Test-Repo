c6e9ba134a7af2cab3c941707cc559e851baf2dd
Charltonmckay <156461992+Charltonmckay@users.noreply.github.com>
2024-03-08 18:45:18 +0200

    Here is the Hello function verion 3

---
+++ Filefive.js
@@ -0,0 +1,14 @@
+function Hello(name) {
+    if (name === undefined || name === null){
+    console.log("Hey Friendly Ghost")
+    }else{
+
+    console.log("Hello", name + "  I'm Coding and Pushing");
+
+    }
+}
+
+Hello()
+Hello("Daniel")
+Hello("Lucian")
+Hello("Dunyiswa")
