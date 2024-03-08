be75462eb7f30ef7ccc9506928b11a9cb2a2adb5
Charltonmckay <156461992+Charltonmckay@users.noreply.github.com>
2024-03-08 18:13:04 +0200

    This is the Hello function

---
+++ Thirdfile.js
@@ -0,0 +1,15 @@
+function Hello (name) {
+    if (name === undefined|| name === null ) {
+    console.log("Hello Casper...");
+    }else{
+
+        
+    console.log  ("Hello", name + "!");
+    }
+}
+
+Hello(Charlton)
+Hello()
+Hello(Daniel) 
+Hello(Lucian)
+Hello(Dunyiswa)
