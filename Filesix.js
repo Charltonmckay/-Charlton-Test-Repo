58897b346039197c8f58713150c9d07da517b539
Charltonmckay <156461992+Charltonmckay@users.noreply.github.com>
2024-03-08 20:08:39 +0200

    Here is Hello version 4

---
+++ Filesix.js
@@ -0,0 +1,14 @@
+function Hello(name) {
+    if(name === undefined  || name ===0 ){
+        console.log("Hello Casper")
+    }else{
+        console.log("Hey", name + " I'm pushing Code");
+    }
+
+}
+
+Hello()
+Hello("Dunyswa")
+Hello("Lucian")
+Hello("Charlton")
+
